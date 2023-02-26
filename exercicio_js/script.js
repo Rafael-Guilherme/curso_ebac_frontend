d88b7296929165404ee14426b75cc3a168d0a07a
Rafael <rafael_guilherme@hotmai.com>
2023-02-26 07:57:24 -0300

    fix exercicio

--- exercicio_js/script.js
+++ exercicio_js/script.js
@@ -1,26 +1,31 @@
 const form = document.getElementById('form-cadastro')
-const idade = document.getElementById('idade')
-const nascimento = document.getElementById('nascimento')
+const numA = document.getElementById('numA')
+const numB = document.getElementById('numB')
 const sucesso = document.querySelector('#sucesso')
-const campoNascimento = document.querySelector('#campo_nascimento')
-const msgError = document.getElementById('mensagem-erro')
-const idadeError = document.getElementById('erro-idade')
-const currentYear = new Date().getFullYear();
+//const campoNascimento = document.querySelector('#campo_nascimento')
+//const msgError = document.getElementById('mensagem-erro')
+const numAError = document.getElementById('erro-numA')
+//const currentYear = new Date().getFullYear();
 
 
 form.addEventListener('submit', function(e) {
   e.preventDefault()
 
-  if (Number(idade.value) < Number(nascimento.value)) {
-    validar()
-    idadeError.style.display = 'none'
-    idade.style.border = ''
+  if (Number(numA.value) < Number(numB.value)) {
+    //validar()
+    numAError.style.display = 'none'
+    numA.style.border = ''
+
+    sucesso.style.display = 'block'
+    numA.value = ''
+    numB.value = ''
+    //campoNascimento.style.display = 'none'
   } else {
-    idadeError.style.display = 'block'
-    idade.style.border = '1px solid red'
+    numAError.style.display = 'block'
+    numA.style.border = '1px solid red'
     sucesso.style.display = 'none'
-    msgError.style.display = 'none'
-    nascimento.style.border = ''
+    //msgError.style.display = 'none'
+    //nascimento.style.border = ''
   }
 })
 
@@ -28,41 +33,41 @@ form.addEventListener('submit', function(e) {
 //e que a idade e o ano são correspondentes.
 //Estando correta, já temos a segunda validação,
 //afinal o ano será sempre maior que a idade.
-function validateCampoNascimento() {
-  const nascimentoValue = nascimento.value;
+// function validateCampoNascimento() {
+//   const nascimentoValue = nascimento.value;
 
-  if (nascimentoValue.length !== 4) {
-    msgError.style.display = 'block'
-    nascimento.style.border = '1px solid red'
-    return false;
-  }
+//   if (nascimentoValue.length !== 4) {
+//     msgError.style.display = 'block'
+//     nascimento.style.border = '1px solid red'
+//     return false;
+//   }
 
-  const idadeValue = currentYear - nascimentoValue;
+//   const idadeValue = currentYear - nascimentoValue;
   
-  if (idade.value !== idadeValue.toString()) {
-    msgError.style.display = 'block'
-    nascimento.style.border = '1px solid red'
-    return false;
-  }
+//   if (idade.value !== idadeValue.toString()) {
+//     msgError.style.display = 'block'
+//     nascimento.style.border = '1px solid red'
+//     return false;
+//   }
 
-  msgError.style.display = 'none'
-  nascimento.style.border = ''
-  sucesso.style.display = 'none'
-  return true;
-}
+//   msgError.style.display = 'none'
+//   nascimento.style.border = ''
+//   sucesso.style.display = 'none'
+//   return true;
+// }
 
-//Evento quando submeter o form previne o reload da página
-//verifica se a função é true or false.
-function validar() {
+// //Evento quando submeter o form previne o reload da página
+// //verifica se a função é true or false.
+// function validar() {
 
-    if (validateCampoNascimento()) {
-        sucesso.style.display = 'block'
-        idade.value = ''
-        nascimento.value = ''
-        campoNascimento.style.display = 'none'
-    } else {
-        sucesso.style.display = 'none'
-        nascimento.style.border = '1px solid red'
-        campoNascimento.style.display = 'block'
-    }
-}
+//     if (validateCampoNascimento()) {
+//         sucesso.style.display = 'block'
+//         idade.value = ''
+//         nascimento.value = ''
+//         campoNascimento.style.display = 'none'
+//     } else {
+//         sucesso.style.display = 'none'
+//         nascimento.style.border = '1px solid red'
+//         campoNascimento.style.display = 'block'
+//     }
+// }
