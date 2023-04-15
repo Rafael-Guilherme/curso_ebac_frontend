40140c4e608b93d0824eb7e7e43bc1bc753f9312
Rafael Guilherme <71350127+Rafael-Guilherme@users.noreply.github.com>
2023-04-15 18:00:27 -0300

    Add files via upload
    exercicio mod26

---
+++ exercicio_ts.ts
@@ -0,0 +1,10 @@
+const mult = (a: number, b: number) => {
+    return a * b
+}
+
+const saudacao = (nome: string) => {
+    return (`Olá ${nome}`)
+}
+
+console.log(mult(10, 20))
+console.log(saudacao("Rafael"))
